a5961d325ebd819a3603768c028f7abd69a9d570
Matt Schnider <matt.schnider@gmail.com>
2019-01-07 18:47:41 -0500

    testing repo

---
+++ TestApp/testApp/testApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test Application");
+        }
+    }
+}
