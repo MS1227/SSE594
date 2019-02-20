32fc0523bcfb0d392159a88e6253bf2ca6f0bd58
Jake-Foster <35465022+Jake-Foster@users.noreply.github.com>
2019-02-19 20:08:59 -0500

    NUnit Test Fixed

---
+++ NUnitTestProject1/RoomsTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoomsType;
+
+namespace RoomsTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Rooms myRooms = new Rooms();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
