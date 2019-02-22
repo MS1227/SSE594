e13b39609eaed98c70e69325a20eb6b474a0f6df
Jake-Foster <35465022+Jake-Foster@users.noreply.github.com>
2019-02-21 21:24:12 -0500

    Dictionary of users added to server class

--- ClassLibrary1/Server.cs
+++ ClassLibrary1/Server.cs
@@ -6,10 +6,35 @@ namespace Server
     public class Server
     {
         private Dictionary<string, ChatRoom> myChatRooms;
+        private Dictionary<string, User> users;
 
         public Server()
         {
             myChatRooms = new Dictionary<string, ChatRoom>();
+        }
+
+        public bool addUser(User user)
+        {
+            //myUsers.Add(new User((short)myUsers.Count, name));
+            if (!users.ContainsKey(user.getName()))
+            {
+                users.Add(user.getName(), user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public User getUser(string name)
+        {
+            return users[name];
+        }
+
+        public int getNumUsers()
+        {
+            return users.Count;
         }
 
         public bool createChatRoom(string name)
